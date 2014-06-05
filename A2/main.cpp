e071e4d973d228d6d6449b0b5c41f4fffe072d3b
amirhakim <amirhakim@gmail.com>
2014-06-05 18:10:52 -0400

    Initial Commit

---
+++ A2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  A2
+//
+//  Created by AMIR HAKIM on 2014-06-05.
+//  Copyright (c) 2014 AMIR HAKIM. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
